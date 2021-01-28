77f370655001a37cdb92b5c22853c081610145a7
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2021-01-28 15:08:55 -0800

    added svg background

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,13 +2,15 @@ import React from 'react';
 import '../../App.css';
 
 const Footer = () => { 
+    const today = new Date();
         return (
         <div>
             <div className="main-footer">
                 <div className="container">
                     <div className="row">
                         <div className="col-12 pt-4">
-                            <p>made with <span className="footer-heart">&hearts;</span> by me</p>
+                            <p>made with <span className="footer-heart">&hearts;</span> by nick giegerich</p>
+                            <p><span className="">&copy;</span> {today.getFullYear()}</p>
                         </div>
                     </div>
                 </div>
